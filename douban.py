1084dbebd670e8db9c0bf4a49477de2007459f01
zhaobo <azhaobo1991@163.com>
2017-09-26 00:17:30 +0800

    豆瓣读书爬虫，获取top250

--- douban.py
+++ douban.py
@@ -1,22 +1,31 @@
-﻿import urllib.request
+﻿from urllib import parse,request
 from bs4 import BeautifulSoup
 import sys
 import re
 
-print(sys.getfilesystemencoding())
+
 index = {'icn' : 'index-book250-all'}
 url = 'https://book.douban.com/top250'
 
 
-req = urllib.request.Request(url,headers = index)
-html = urllib.request.urlopen(req).read().decode('utf-8')
-bs_obj = BeautifulSoup(html, 'html.parser')
-links = bs_obj.findAll('div', {'class' : 'pl2'})
-# for link in links:
-# 	for lc in link.contents:
-# 		print(lc.string)
-with open('douban_book.txt','w', encoding = 'UTF-8') as file_obj:
-	for link in links:
-		for lc in link.contents:
-			if lc.string is not None:
-				file_obj.write(lc.string)
+
+def get_title(url, index):
+	data = parse.urlencode(index)
+	req = request.Request(url + '?' + data)
+	print(req.get_full_url())
+	html = request.urlopen(req).read().decode('utf-8')
+	bs_obj = BeautifulSoup(html, 'html.parser')
+	links = bs_obj.findAll('a', href = re.compile('.*subject.*'))
+
+	with open('douban_book.txt','a', encoding = 'UTF-8') as file_obj:
+		for link in links:
+			# desc_name(link.stripped_strings)
+			if link.stripped_strings is not None and len(list(link.stripped_strings)) > 0:
+				for string in link.stripped_strings:
+					file_obj.write(string )
+				file_obj.write('\n')
+
+for val in range(0,10):
+	if val != 0:
+		index = {'start' : val * 25}
+	get_title(url, index)
