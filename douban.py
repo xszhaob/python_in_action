f6ec8ae63266c3b186030e9960785dbf92a26f1d
zhaobo <azhaobo1991@163.com>
2017-10-02 23:09:16 +0800

    Python数据采集 练习

--- douban.py
+++ douban.py
@@ -1,10 +1,15 @@
 ﻿from urllib import parse,request
 from bs4 import BeautifulSoup
 import re
+import csv
+import io
+import sys
+import os
 
 
 index = {'icn' : 'index-book250-all'}
 url = 'https://book.douban.com/top250'
+file_path = 'F:\douban\douban_book.csv'
 
 # 获取书名 评分 描述
 def get_title(url, index):
@@ -19,6 +24,7 @@ def get_title(url, index):
 		desc = get_desc(link)
 		star = get_star(link)
 		print(book_name + '  ' + star + '  ' + desc)
+		store_in_csv(book_name, star, desc)
 
 
 # 获取书名
@@ -37,7 +43,25 @@ def get_star(link):
 	star_div = link.find_next_siblings('div', {'class' : 'star clearfix'})[0]
 	return star_div.findAll('span', {'class' : 'rating_nums'})[0].get_text()
 
+# 保存在csv文件中
+def store_in_csv(book_name, star, desc):
+	with open(file_path, 'a', encoding = 'gb18030', newline='') as file_obj:
+		writer = csv.writer(file_obj, dialect='excel')
+		writer.writerow([book_name, star, desc])
+
+# 设置表头
+def init_title():
+	with open(file_path, 'a', encoding = 'gb18030', newline='') as file_obj:
+		writer = csv.writer(file_obj, dialect='excel')
+		writer.writerow(['书名', '评分', '描述'])
+
+
 # 执行
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
+if os.path.exists(file_path):
+	os.remove(file_path)
+
+init_title()
 for val in range(0,10):
 	if val != 0:
 		index = {'start' : val * 25}
